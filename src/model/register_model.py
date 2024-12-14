2cc040b93697b589cc6391599f1306dc12c62b23
kartiksonaghela <kbs.kartik@gmail.com>
2024-12-14 21:20:20 +0530

    changes done

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("ec2-13-201-100-239.ap-south-1.compute.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-13-201-100-239.ap-south-1.compute.amazonaws.com:5000")
 
 
 # logging configuration
